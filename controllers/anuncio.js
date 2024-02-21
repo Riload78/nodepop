8c3054cfd528631b52e8b196806ab14739274c73
Ricardo Lopez <rlopez@next-ecommerce.com>
2024-02-22 00:20:42 +0100

    List, Create, List Tags

--- controllers/anuncio.js
+++ controllers/anuncio.js
@@ -3,9 +3,30 @@ const Anuncio = require('../models/anuncioSchema')
 const getAnuncios = async () => {
   try {
     const anunciosDB = await Anuncio.find()
-    return anunciosDB
+    return { status: 'success', data: anunciosDB }
   } catch (error) {
     console.log(`Error al obtener los anuncios de la base de datos: ${error}`)
+    return error
   }
 }
-module.exports = getAnuncios
+
+const getTags = async () => {
+  try {
+    const tags = await Anuncio.distinct('tags')
+    return { status: 'success', data: tags }
+  } catch (error) {
+    return error
+  }
+}
+
+const createAnuncio = async (data) => {
+  const newAnuncio = new Anuncio(data)
+  try {
+    await newAnuncio.save()
+    return { status: 'success', msg: 'El anuncio se ha creado correctamente' }
+  } catch (error) {
+    console.log(`Hubo un error al guardar el anuncio en la BD: ${error}`)
+    return error
+  }
+}
+module.exports = { getAnuncios, getTags, createAnuncio }
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const getAnuncios = require('../controllers/anuncio')
+const { getAnuncios, getTags, createAnuncio } = require('../controllers/anuncio')
 
 /* GET users listing. */
 router.get('/', async (req, res, next) => {
@@ -9,12 +9,36 @@ router.get('/', async (req, res, next) => {
 
 router.get('/anuncios', async (req, res, next) => {
   try {
-    // const anuncios = await Anuncio.find()
-    const data = await getAnuncios()
-    console.log(data)
+    const result = await getAnuncios()
+    res.send(result)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+router.post('/anuncios', async (req, res, next) => {
+  const data = req.body
+  console.log(data)
+  try {
+    const result = await createAnuncio(data)
+
+    if (!result.status) return res.status(400).json(result.errors)
+    res.send(result)
+  } catch (error) {
+    console.error(error)
+    next(error)
+  }
+})
+
+router.get('/tags', async (req, res, next) => {
+  try {
+    const tags = await getTags()
+    if (!tags) throw new Error('Error al obtener las etiquetas')
+    const data = [{ tags }]
     res.send(data)
   } catch (error) {
     console.log(error)
+    res.status(500).json({ error: 'Ocurrió un error al obtener las etiquetas' })
   }
 })
 
