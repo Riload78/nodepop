738816deeafc08d05995ba3f52b433d5c2bd3f36
Ricardo Lopez <rlopez@next-ecommerce.com>
2024-05-17 17:07:44 +0200

    Customer account struture

--- controllers/CustomerAccountController.js
+++ controllers/CustomerAccountController.js
@@ -1,9 +1,13 @@
-const index = (req, res) => {
-  res.locals.subtitle = res.__('Customer Account')
-  res.locals.error = ''
-  res.locals.email = ''
+const index = async (req, res, next) => {
+  try {
+    res.locals.subtitle = res.__('Customer Account')
+    res.locals.error = ''
+    res.locals.email = ''
 
-  res.render('customer-account', { title: 'NODEPOP' })
+    res.render('customer-account', { title: 'NODEPOP' })
+  } catch (error) {
+    next(error)
+  }
 }
 
 module.exports = { index }
